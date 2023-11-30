3bf98b12d9234f7e9c3937df0c2744051b943168
Prashantk0997 <Prashantk0997@gmail.com>
2023-11-30 22:01:54 +0530

    added

---
+++ src/components/NewsItem.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const NewsItem = (props)=> {
+
+    let {title,description,imageUrl,newsUrl,author,date,source} = props;
+    return (
+      <div className="my-4">
+        <div className="card">
+          <div style={
+            {display:'flex',
+             justifyContent:'flex-end',
+             position:'absolute',
+             right:'0',}
+          }>
+          <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img src={!imageUrl?"https://www.vskills.in/certification/blog/wp-content/uploads/2015/01/structure-of-a-news-report.jpg":imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}...</h5>
+              <p className="card-text">{description}...</p>
+              <p className="card-text"><small className="text-body-secondary">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+              <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+            </div>
+        </div>
+      </div>
+    )
+  
+}
+
+export default NewsItem
